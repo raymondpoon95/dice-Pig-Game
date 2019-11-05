efb5912fbb183cc2b80dddcdf3f4a04d9c0eb1c7
Raymond Poon <raymondpoon95@hotmail.co.uk>
2019-11-05 03:55:14 +0000

    completed input feature

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
             nextPlayer();
         }
 
-        previousDiceRoll = dice;
+        previousDiceRoll = dice; // variable to keep note of previous dice roll
     }
 });
 
@@ -54,7 +54,16 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 
         document.querySelector('#score-'+activePlayer).textContent = scores[activePlayer];
 
-        if(scores[activePlayer] >= 100){ // change this back to 100
+        let input = document.querySelector('.goal-score').value; // grab the input
+        let winningScore;
+
+        if(input){
+            winningScore = input;
+        } else {
+            winningScore = 100;
+        }
+
+        if(scores[activePlayer] >= winningScore){ 
             document.querySelector('#name-'+activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
